b3a7a13549770560144ccc75654c1114fc6a5287
sw-alpha-romeo <alpha.romeo@southwinds.io>
2022-11-10 06:48:33 +0000

    modified code to add steps for building patching package using new approach

--- release/debian.go
+++ release/debian.go
@@ -15,6 +15,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/magiconair/properties"
 	"gopkg.in/yaml.v2"
 	"southwinds.dev/artisan/build"
 	"southwinds.dev/artisan/core"
@@ -89,8 +90,26 @@ func BuildDebianPackage(pkgNames []string, opts *ExportOptions) error {
 		return fmt.Errorf("failed to read .deb file from path %s, \n error is %s", patchFolder, err)
 	}
 
+	err = buildMetadata(patchFolder)
+	if err != nil {
+		os.RemoveAll(tmp)
+		return fmt.Errorf("failed to build metadata file for debian packages, \n error is %s", err)
+	}
+
+	err = generateShellScript(targetFolder)
+	if err != nil {
+		os.RemoveAll(tmp)
+		return fmt.Errorf("failed to generate shell script for debian patch application, \n error is %s", err)
+	}
+
+	ver, err := getOsInfo(patchFolder)
+	if err != nil {
+		os.RemoveAll(tmp)
+		return fmt.Errorf("failed to get host's operating system version, \n error is %s", err)
+	}
+
 	// generate build function build.yaml for this package
-	bfBytes, err := generateBuildFunctions(bckupCmds)
+	bfBytes, err := generateBuildFunctions(bckupCmds, ver)
 	if err != nil {
 		return fmt.Errorf("failed to marshall debian package build file: %s", err)
 	}
@@ -229,14 +248,8 @@ func aptPkgName() string {
 	return fmt.Sprintf("127.0.0.1/os/packages/apt:%d", time.Now().Unix())
 }
 
-func generateBuildFunctions(bckupCmds []string) ([]byte, error) {
+func generateBuildFunctions(bckupCmds []string, targetOs string) ([]byte, error) {
 	export_yes := true
-	export_no := false
-	patchCmd := "(cd patch && echo \"\" && echo \"************ patching started ***************\"" +
-		" && sudo DEBIAN_FRONTEND=noninteractive dpkg --force-all -i * && echo \"************ patch completed ***************\" && echo \"\")"
-	rollbackCmd := "(cd backup && echo \"\" && echo \"************ patching failed, rollback started ***************\"" +
-		" && sudo DEBIAN_FRONTEND=noninteractive dpkg --force-all -i * && echo \"************ rollback completed ***************\" && echo \"\")"
-	combinedCmd := "bash -c '" + patchCmd + " || " + rollbackCmd + "'"
 	bf := data.BuildFile{
 		Runtime: "ubi-min",
 		Functions: []*data.Function{
@@ -245,16 +258,16 @@ func generateBuildFunctions(bckupCmds []string) ([]byte, error) {
 				Description: "apply debian packages to the operating system. In case of failure, will automatically rollback",
 				Export:      &export_yes,
 				Run: []string{
-					"bash -c 'mkdir -p backup'",
-					"$(backup)",
-					combinedCmd,
+					"bash -c './patch-apt.sh' -v " + targetOs,
 				},
 			},
 			{
-				Name:        "backup",
-				Description: "take backup of current version of packages",
-				Export:      &export_no,
-				Run:         bckupCmds,
+				Name:        "apply",
+				Description: "apply debian packages to the operating system. In case of failure, will automatically rollback",
+				Export:      &export_yes,
+				Run: []string{
+					"bash -c './patch-apt.sh' -s yes -v " + targetOs,
+				},
 			},
 		},
 	}
@@ -355,3 +368,459 @@ func dedup(dep string) (string, error) {
 	}
 	return strings.TrimSpace(b.String()), nil
 }
+
+func generateShellScript(targetFolder string) error {
+	er := os.WriteFile(filepath.Join(targetFolder, "patch-apt.sh"), []byte(shellScript), 0755)
+	return er
+}
+
+func getOsInfo(executionPath string) (string, error) {
+
+	cmd := "cat /etc/os-release"
+	core.DebugLogger.Printf("using command to find host operating system version :%s\n ", cmd)
+	// using async because when using sync occassionally it been observed that return list contains
+	// status messages key word also like "processing" along with the dependent package names
+	output, err := build.ExeAsync(cmd, executionPath, merge.NewEnVarFromSlice([]string{}), false)
+	// note: the dep list will contain the parent package name also for which we looked dependencies
+	if err != nil {
+		return "", err
+	}
+
+	p := properties.MustLoadString(output)
+	id := p.MustGetString("ID")
+	version_id := p.MustGetString("VERSION_ID")
+	tagetOsVersion := id + "-" + version_id
+	replacer := strings.NewReplacer("\"", "")
+	tagetOsVersion = replacer.Replace(tagetOsVersion)
+	return tagetOsVersion, err
+}
+
+func buildMetadata(patchFolder string) error {
+	cmd := "bash -c 'dpkg-scanpackages . /dev/null > Release'"
+	_, er := build.Exe(cmd, patchFolder, merge.NewEnVarFromSlice([]string{}), false)
+	if er != nil {
+		return er
+	}
+
+	cmd = "bash -c 'dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz'"
+	_, er = build.Exe(cmd, patchFolder, merge.NewEnVarFromSlice([]string{}), false)
+
+	return er
+}
+
+const shellScript = `
+#!/usr/bin/env bash
+DEB_DIR=/tmp/patch
+APT_DIR=/etc/apt
+SNAP_DEVICE=""
+STD=$(date +"%Y/%m/%d at %H:%M:%S")
+SNAPS_ENABLED=""
+SNAP_LV_NAME=tsbackup
+SNAP_DEV_SIZE=6
+SNAP_NO_TO_KEEP=1
+OS_ID=$(grep "^ID=" /etc/os-release | awk -F"=" '{print $2}')
+VERSION_ID=$(grep "VERSION_ID" /etc/os-release | awk -F"=" '{print $2}' | sed -e 's/^"//' -e 's/"$//')
+OS_RELEASE="$OS_ID-$VERSION_ID"
+TARGET_OS=""
+#DISTRIB_ID=$(grep DISTRIB_ID /etc/lsb-release | awk -F"=" '{print $2}')
+
+#################  Functions  ############################################
+
+check_timeshift() {
+    if [ ! $(command -v timeshift) ]; then
+    echo "########################################################"
+    echo ""
+    echo "Timeshift not available!"
+    echo ""
+    echo "Timeshift package not installed or not in the PATH."
+    echo ""
+    echo "########################################################"
+    exit 1
+    fi
+}
+
+check_lvm() {
+    if [ ! $(command -v lvs) ]; then
+    echo "########################################################"
+    echo ""
+    echo "LVM not available!"
+    echo ""
+    echo "LVM package not installed or not in the PATH."
+    echo ""
+    echo "########################################################"
+    fi
+}
+
+snapshot_number() {
+## Check the numer of timeshift snapshots
+    sudo timeshift --list --snapshot-device ${SNAP_DEVICE} |grep 'No snapshots on this device' > /dev/null
+    if [ $? -eq 0 ]; then
+        SNAP_NO=0
+    else
+        SNAP_NO=$(sudo timeshift --list  --snapshot-device ${SNAP_DEVICE}  | grep snapshots | awk '{print $1}')
+    fi
+}
+
+oldes_snapshot() {
+## Find snapshot name
+    SNAP_NAME=$(sudo timeshift --list --snapshot-device ${SNAP_DEVICE} |grep Num -A2 | tail -n 1 |awk '{print $3}')
+}
+
+delete_snapshot() {
+## Delete the snapshot
+    echo "########################################################"
+    echo ""
+    echo "Deleting ${SNAP_NAME}"
+    echo ""
+    echo "########################################################"
+    sudo timeshift --delete --snapshot ${SNAP_NAME} --snapshot-device ${SNAP_DEVICE}
+}
+
+delete_oldest_snapshot() {
+## Delete the snapshot
+    echo "########################################################"
+    echo ""
+    echo "Deleting ${SNAP_NAME}"
+    echo ""
+    echo "########################################################"
+    sudo timeshift --delete --snapshot ${SNAP_NAME} --snapshot-device ${SNAP_DEVICE}
+}
+
+create_snapshot() {
+    sudo timeshift --create --comments "before patching ${STD}" --snapshot-device ${SNAP_DEVICE}
+    CREATE_SNAP_STATUS=$?
+    sudo timeshift --list  --snapshot-device ${SNAP_DEVICE}  | grep "${STD}"
+    CHECK_SNAP_STATUS=$?
+    if [ $CREATE_SNAP_STATUS -eq 0 ] && [ $CHECK_SNAP_STATUS -eq 0 ]; then
+        NEW_SNAP_NAME=$(sudo timeshift --list  --snapshot-device ${SNAP_DEVICE}  | grep "${STD}" | awk '{print $3}')
+        echo "${SNAP_DEVICE};${NEW_SNAP_NAME}"| sudo tee $HOME/.timeshift_snap_info
+        echo "########################################################"
+        echo ""
+        echo "Timeshift snapshot successfully created"
+        echo ""
+        echo "########################################################"
+    else
+        echo "########################################################"
+        echo ""
+        echo "Timeshift snapshot not created!"
+        echo ""
+        echo "########################################################"
+        exit 1
+    fi
+}
+
+apply_patch() {
+## Copy the patch tirectory to ${DEB_DIR} to avoid permission issue
+
+    cp -r patch ${DEB_DIR}
+
+## Make sure Packages and Relese have correct access
+    sudo chown -R _apt: ${DEB_DIR}
+    sudo chmod a+r ${DEB_DIR}/Packages.gz
+    sudo chmod a+r ${DEB_DIR}/Release
+
+## Make a copy of the sources.list file
+
+    sudo mv ${APT_DIR}/sources.list ${APT_DIR}/sources.list.bkp
+
+## Create a new sources list file
+
+    sudo echo "deb [trusted=yes] file:${DEB_DIR} ./" | sudo tee ${APT_DIR}/sources.list
+
+## Patch the system
+
+    sudo apt-get update \
+    && sudo  DEBIAN_FRONTEND=noninteractive apt-get -qy \
+    -o 'Dpkg::Options::=--force-confdef' -o 'Dpkg::Options::=--force-confold' upgrade -y
+
+    if [ $? -eq 0 ]; then
+        echo "########################################################"
+        echo ""
+        echo "Successfully patched"
+        echo ""
+        echo "########################################################"
+    else
+        echo "########################################################"
+        echo ""
+        echo "Patching failed! Check system logs"
+        echo ""
+        echo "########################################################"
+        sudo mv ${APT_DIR}/sources.list.bkp ${APT_DIR}/sources.list
+        exit 1
+    fi
+  
+## Restore the sources.list file
+
+    sudo mv ${APT_DIR}/sources.list.bkp ${APT_DIR}/sources.list
+
+## Delete DEB_DIR
+
+    sudo rm -rf ${DEB_DIR} 
+}
+
+usage() {
+      echo "Usage: $0 [ -s yes ] [ -v os_version ] [ -d snapshot_device ]" 1>&2
+    }
+    
+exit_abnormal() {
+     usage
+     exit 1
+    }
+
+fs_type() {
+    FS_TYPE=$(sudo lsblk -f $SNAP_DEVICE -o FSTYPE | tail -n1)
+}
+
+fs_size() {
+    if [[ "$FS_TYPE" =~ "ext" ]]; then
+        B_SIZE=$(sudo tune2fs -l $SNAP_DEVICE | grep 'Block size' | awk -F ":" '{print $2}' | xargs)
+        FREE_B_COUNT=$(sudo tune2fs -l $SNAP_DEVICE |  grep 'Free blocks' | awk -F ":" '{print $2}' | xargs)
+        SNAP_DEV_FREE=$(echo "$B_SIZE*$FREE_B_COUNT/1073741824" | bc -l)   
+    elif [[ "$FS_TYPE" =~ "xfs" ]]; then
+        B_SIZE=$(sudo xfs_info $SNAP_DEVICE | grep ^data. | awk '{print $3}' | awk -F"=" '{print $2}' | sed -e 's/^,//' -e 's/,$//')
+        FREE_B_COUNT=$(sudo xfs_db -r "-c freesp -s" $SNAP_DEVICE |grep "total free blocks" | awk '{ print $4 }')
+        SNAP_DEV_FREE=$(echo "$B_SIZE*$FREE_B_COUNT/1073741824" | bc -l)
+    else
+        echo "########################################################"
+        echo ""
+        echo "Unsupported FS type. Use EXT or XFS"
+        echo ""
+        echo "########################################################"
+        Exit 1
+    fi    
+}
+
+#################  Functions  ############################################
+
+## Perform OS patching with or without timeshift snapshot
+## To enable timeshift snapshoting use '-s yes' option 
+
+while getopts ":s:v:d:" options; do
+    
+      case "${options}" in
+        s)
+          SNAPS_ENABLED=$(echo ${OPTARG} | tr '[:upper:]' '[:lower:]')
+          if [ ${SNAPS_ENABLED} != 'yes' ]; then
+             echo "Error: 'yes' required with -s"
+             exit_abnormal
+             exit 1
+         
+           fi
+          ;;
+        v)
+          TARGET_OS=$(echo ${OPTARG} )
+          if [ ${TARGET_OS} = '' ]; then
+             echo "Error: 'os version' required with -v"
+             exit_abnormal
+             exit 1
+         
+           fi
+          ;;
+        d)
+          SNAP_DEVICE=$(echo ${OPTARG} )
+          if [ ${SNAP_DEVICE} = '' ]; then
+             echo "Error: 'snapshot device required with -d"
+             exit_abnormal
+             exit 1
+         
+           fi
+          ;;
+        :)
+          echo "Error: -${OPTARG} requires an argument."
+          exit_abnormal
+          ;;
+        *)
+          exit_abnormal
+          ;;
+      esac
+    done
+
+## Make sure this is a Debian like system
+
+if [ ! -f /etc/debian_version ] && [ ! -f /etc/lsb-release ]; then
+echo "########################################################"
+echo ""
+echo "OS distribution mismatch!"
+echo ""
+echo "The package should be executed on a Debian like system!"
+echo ""
+echo "########################################################"
+exit 1
+fi
+
+## Make sure distribution version matches
+ 
+if [ $TARGET_OS != $OS_RELEASE ]; then
+echo "########################################################"
+echo ""
+echo "OS version mismatch!"
+echo ""
+echo "The package has been prepared for $TARGET_OS and is executed on $OS_RELEASE"
+echo ""
+echo "########################################################"
+exit 1
+fi
+
+#################  Main script  ############################################
+
+if [ ! -z ${SNAP_DEVICE} ] && [ -z ${SNAPS_ENABLED} ]; then
+    echo "########################################################"
+    echo ""
+    echo "Option [ -d snapshot_device ] must be used with [ -s yes ]"
+    echo ""
+    echo "########################################################"
+    exit 1
+fi
+
+if [ ${SNAPS_ENABLED} = 'yes' ] && [ ! -z ${SNAP_DEVICE} ]; then
+    fs_type
+    fs_size 
+    if (( $(echo "$SNAP_DEV_FREE > $SNAP_DEV_SIZE" | bc -l) )); then
+        ## Make sure timeshift has been installed on the system
+        check_timeshift
+        echo "########################################################"
+        echo ""
+        echo "Patching with timeshift snapshot"
+        echo ""
+        echo "########################################################"
+        snapshot_number
+        ## If SNAP_NO > SNAP_NO_TO_KEEP remove the oldes timeshift snapshot
+        if [ ${SNAP_NO} -gt ${SNAP_NO_TO_KEEP} ]; then
+            oldes_snapshot
+            delete_oldest_snapshot
+        fi
+        ## Create snapshot with timeshift
+        create_snapshot
+        ## Patch the system
+        apply_patch
+    else
+        echo "########################################################"
+        echo ""
+        echo "Not enough free disk space on $SNAP_DEVICE - at least "$SNAP_DEV_SIZE"G required"
+        echo ""
+        echo "Choose another device or add free space to the device"
+        echo ""
+        echo "########################################################"
+        exit 1        
+   fi
+
+elif [ ${SNAPS_ENABLED} = 'yes' ] && [ -z ${SNAP_DEVICE} ]; then
+
+    ## Make sure timeshift has been installed on the system
+
+    check_timeshift
+
+    echo "########################################################"
+    echo ""
+    echo "Patching with timeshift snapshot"
+    echo ""
+    echo "########################################################"
+
+    ## Check if LVM is on the system
+    command -v lvs > /dev/null
+    if [ $? -ne 0 ] || [ $(sudo vgs --noheadings | wc -l) -eq 0 ]; then
+        PWD_SIZE=$(df --output=avail -B 1 "$PWD" | tail -n 1 | numfmt --to="iec" | sed 's/[A-Za-z]*//g')
+    ## Check if there is enough disk space on the working directory device
+        if (( $(echo "$PWD_SIZE > $SNAP_DEV_SIZE" | bc -l) )); then 
+            SNAP_DEVICE=$(df $PWD --output=source | tail -n 1)
+            ## Check the numer of timeshift snapshots
+            snapshot_number
+            ## If SNAP_NO > SNAP_NO_TO_KEEP remove the oldes timeshift snapshot
+            if [ ${SNAP_NO} -gt ${SNAP_NO_TO_KEEP} ]; then
+                oldes_snapshot
+                delete_oldest_snapshot
+            fi
+            ## Create snapshot with timeshift
+            create_snapshot
+            ## Patch the system
+            apply_patch
+                    
+        else 
+            SNAP_DEVICE=$(df $PWD --output=source | tail -n 1)
+            echo "########################################################"
+            echo ""
+            echo "Not enough free disk space on $SNAP_DEVICE to create timeshift snapshot and LVM not installed."
+            echo ""
+            echo "Please create a filesystem for timeshift snapshots."
+            echo ""
+            echo "########################################################"
+            exit 1
+        ## Check if SNAP_FS exists on the system
+        fi
+    else
+        sudo lvs |grep ${SNAP_LV_NAME} > /dev/null
+        if [ $? -eq 0 ]; then
+            SNAP_DEVICE=/dev/mapper/$(sudo lvs |grep ${SNAP_LV_NAME} |awk '{print $2}')-${SNAP_LV_NAME}     
+            ## Check the numer of timeshift snapshots
+            snapshot_number
+            ## If SNAP_NO > SNAP_NO_TO_KEEP remove the oldes timeshift snapshot
+            if [ ${SNAP_NO} -gt ${SNAP_NO_TO_KEEP} ]; then
+                oldes_snapshot
+                delete_oldest_snapshot
+            fi
+            ## Create snapshot with timeshift
+            create_snapshot
+            ## Patch the system
+            apply_patch
+
+        else
+            ## Check if there is enough free space in PWD
+            FREE_SPACE=$(df --output=avail -B 1 "$PWD" | tail -n 1 | numfmt --to="iec" | sed 's/[A-Za-z]*//g')
+            if (( $(echo "$FREE_SPACE > $SNAP_DEV_SIZE" |bc -l) )); then
+                SNAP_DEVICE=$(df $PWD --output=source | tail -n 1)
+                ## Check the numer of timeshift snapshots
+                snapshot_number
+                ## If SNAP_NO > 2 remove the oldes timeshift snapshot
+                if [ ${SNAP_NO} -gt ${SNAP_NO_TO_KEEP} ]; then
+                    oldes_snapshot
+                    delete_oldest_snapshot
+                fi
+                ## Create snapshot with timeshift
+                create_snapshot
+                ## Patch the system
+                apply_patch
+            else
+                ## Check if there is enough free space in vg to create a volume
+                VG_FREE_SIZE=$(sudo vgs --units G --no-suffix --noheadings -o vg_name,vg_free |sort -k 2 -r |head -n1 | awk '{print $2}')
+                VG_NAME=$(sudo vgs --units G --no-suffix --noheadings -o vg_name,vg_free |sort -k 2 -r |head -n1 | awk '{print $1}')      
+                if (( $(echo "$VG_FREE_SIZE > $SNAP_DEV_SIZE" |bc -l) )); then
+                    sudo lvcreate -n ${SNAP_LV_NAME} -L ${SNAP_DEV_SIZE}G ${VG_NAME}
+                    sudo mkfs.ext4 /dev/mapper/${VG_NAME}-${SNAP_LV_NAME}
+                    SNAP_DEVICE=/dev/mapper/${VG_NAME}-${SNAP_LV_NAME}
+                    ## Check the numer of timeshift snapshots
+                    snapshot_number
+                    ## If SNAP_NO > 2 remove the oldes timeshift snapshot
+                    if [ ${SNAP_NO} -gt ${SNAP_NO_TO_KEEP} ]; then
+                        oldes_snapshot
+                        delete_oldest_snapshot
+                    fi
+                    ## Create snapshot with timeshift
+                    create_snapshot
+                    ## Patch the system
+                    apply_patch
+
+                else
+                    echo "########################################################"
+                    echo ""
+                    echo "Not enough free space in VGs to create LV for timeshift snapshot"
+                    echo ""
+                    echo "At least $SNAP_DEV_SIZE GB required"
+                    echo ""
+                    echo "########################################################"
+                    exit 1
+                fi
+            fi
+        fi        
+    fi   
+else
+
+echo "########################################################"
+echo ""
+echo "Patching without timeshift snapshot"
+echo ""
+echo "########################################################"
+
+## Patch the system
+apply_patch
+fi
+`
