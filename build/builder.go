269b0e2063e8c47f8a96eb1cc4d4ec8bef0d6340
swskipper <skipper@southwinds.io>
2023-04-03 10:55:38 +0100

    add search funcs for secrets

--- build/builder.go
+++ build/builder.go
@@ -330,7 +330,8 @@ func (b *Builder) setUniqueIdName(repo *git.Repository) {
 		// get the commit head and add it to the unique reference
 		ref, err := repo.Head()
 		if err != nil {
-			core.RaiseErr("the git repository exists but does not have a commit yet, you need at least one commit before continuing: this is so that a build reference with a commit head can be available within the build context")
+			// the git repo has no commits yet so cannot get a valid commit hashtag
+			hash = ""
 		}
 		b.commit = ref.Hash().String()
 		hash = fmt.Sprintf("-%s", ref.Hash().String()[:10])
